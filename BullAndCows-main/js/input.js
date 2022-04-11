b16b6b9e3235a700cd7327c75e7fd997be30b395
shamrei12 <61946511+shamrei12@users.noreply.github.com>
2022-04-11 17:06:26 +0300

    Removed a bug with the output of the hidden number to the console. Move number added. Fixed some bugs in the code

---
+++ BullAndCows-main/js/input.js
@@ -0,0 +1,52 @@
+let input_value = document.querySelector(".input_value");
+let buffer = 0;
+document
+  .querySelector(".number_place")
+  .addEventListener("click", function (event) {
+    buttonClick(event.target.innerHTML);
+  });
+
+function buttonClick(value){
+    if(isNaN(parseInt(value))){
+        handleSymbol(value);
+    }else{
+        handleNumber(value);
+    }
+    rerenderScreen();
+}
+function handleSymbol(value){
+    switch (value){
+        case "C":
+            buffer = '';
+            currentTotal = 0;
+            previousOperator = null;
+            break;
+        case "←":
+            if(buffer.length === 1){ //if the screen is any single number, always turn it to 0 when deleting
+                buffer = '';
+            }
+            else{
+                buffer = buffer.substring(0,buffer.length-1); //delete the numbers one by one
+            }
+            break;
+        default:
+            handleMath(value);
+            break;
+    }
+}
+
+function handleNumber(value){
+    if(buffer == ""){
+        buffer = value;
+    }else{
+        buffer += value;
+    }
+}
+
+
+function rerenderScreen() {
+  input_value.value = buffer;
+  if(buffer.length > 3){
+      buffer = ''
+  }
+}
